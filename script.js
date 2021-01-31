c59ee6a87f8fac67992ca281279b1f66fe048427
CorentinKowalsky <75315040+CorentinKowalsky@users.noreply.github.com>
2021-01-31 15:29:55 -0100

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,4 @@ function darkMode(){
 function btSecret(){
     var element = document.querySelector('article')
     element.classList.toggle('btSecret');
-}if(btSecret() == true){
-    console,console.log('Hello');
-}else{
-    console,console.log('Bye');
 }
-
